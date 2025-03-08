1bb59f84f1f49ea0fd53e441ffc5b29365d353df
wgibbs322 <wgibbs322@gmail.com>
2025-03-08 03:28:54 -0800

    tele

--- corsConfig.js
+++ corsConfig.js
@@ -1,8 +1,26 @@
 
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//         const allowedOrigins = ['http://127.0.0.1:5500']; // Add your frontend origin(s) here
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true); // Allow the request
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true, // Enable credentials (cookies/auth headers)
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+// };
+
+// export default corsOptions;
+
+
+
 const corsOptions = {
     origin: (origin, callback) => {
         const allowedOrigins = ['http://127.0.0.1:5500']; // Add your frontend origin(s) here
-        if (!origin || allowedOrigins.includes(origin)) {
+        if (!origin || allowedOrigins.includes(origin) || origin === '*') {
             callback(null, true); // Allow the request
         } else {
             callback(new Error('Not allowed by CORS'));
